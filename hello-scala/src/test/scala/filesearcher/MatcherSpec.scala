f39d2eca426bffc746e6c613485e36e39e450586
liviu.ignat <liviu.ignat@check24.de>
2015-03-10 17:42:41 +0100

    Add no Option parameter passed

--- hello-scala/src/test/scala/filesearcher/MatcherSpec.scala
+++ hello-scala/src/test/scala/filesearcher/MatcherSpec.scala
@@ -36,6 +36,15 @@ class MatcherSpec extends FunSpec {
     }
   }
 
+  describe("When matcher is given a path that matches file, but no content filter is passed") {
+    val matcher = new Matcher("json", new File(".\\testfiles\\").getCanonicalPath(), true)
+    val results = matcher.execute()
+
+    it("Should return a list with those files names") {
+      assert(results == List("hello-scala.json"))
+    }
+  }
+
   describe("When matcher is given a path that matches file and content filter") {
     val matcher = new Matcher("txt", new File(".\\testfiles\\").getCanonicalPath(), true, Some("Liviu"))
     val results = matcher.execute()
