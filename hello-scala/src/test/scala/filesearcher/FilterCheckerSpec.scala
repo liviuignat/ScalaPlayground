ff4b161647baf55f520d54e9d888fbc699c31248
liviu.ignat <liviu.ignat@check24.de>
2015-03-10 14:10:55 +0100

    Switch to fun spec

--- hello-scala/src/test/scala/filesearcher/FilterCheckerSpec.scala
+++ hello-scala/src/test/scala/filesearcher/FilterCheckerSpec.scala
@@ -1,25 +1,27 @@
 package filesearcher
 
 import java.io.File
+import org.scalatest.FunSpec
 
-import org.scalatest.FlatSpec
 
 
 /**
  * Created by liviu.ignat on 3/10/2015.
  */
-class FilterCheckerSpec extends FlatSpec {
-  "When passed a list to the filter and a file matches" should
-  "return a list with that file" in {
-    val listOfFiles = List(new FileObject(new File("random")), new FileObject(new File("match")))
-    val matchedFiles = FilterChecker("match") findMatchedFiles(listOfFiles)
-    assert(matchedFiles == List(new FileObject(new File("match"))))
+class FilterCheckerSpec extends FunSpec {
+  describe("When passed a list to the filter and a file matches") {
+    it("Should return a list with that file") {
+      val listOfFiles = List(new FileObject(new File("random")), new FileObject(new File("match")))
+      val matchedFiles = FilterChecker("match") findMatchedFiles(listOfFiles)
+      assert(matchedFiles == List(new FileObject(new File("match"))))
+    }
   }
 
-  "When passed a list with a directory in it" should
-  "never return the directory from it" in {
-    val listOfIOObjects = List(FileObject(new File("file")), DirectoryObject(new File("dir")))
-    val matchedFiles = FilterChecker("dir") findMatchedFiles(listOfIOObjects)
-    assert(matchedFiles.length == 0)
+  describe("When passed a list with a directory in it") {
+    it("Should never return the directory from it") {
+      val listOfFiles = List(new FileObject(new File("random")), new FileObject(new File("match")))
+      val matchedFiles = FilterChecker("match") findMatchedFiles(listOfFiles)
+      assert(matchedFiles == List(new FileObject(new File("match"))))
+    }
   }
 }
--- hello-scala/src/test/scala/filesearcher/MatcherSpec.scala
+++ hello-scala/src/test/scala/filesearcher/MatcherSpec.scala
@@ -2,24 +2,26 @@ package filesearcher
 
 import java.io.File
 
-import org.scalatest.FlatSpec
+import org.scalatest.FunSpec
 
 /**
  * Created by liviu.ignat on 3/10/2015.
  */
-class MatcherSpec extends FlatSpec {
-  "Matcher that is passed a file matching the filter" should
-  "return a list with that file name" in {
-    val matcher = new Matcher("fake", "fakePath")
-    val results = matcher.execute()
-    assert(results == List("fakePath"))
+class MatcherSpec extends FunSpec {
+  describe("When matcher that is passed a file matching the filter") {
+    it("Should return a list with that file name") {
+      val matcher = new Matcher("fake", "fakePath")
+      val results = matcher.execute()
+      assert(results == List("fakePath"))
+    }
   }
 
-  "Matcher with sub folder checking matching a root location with two subtree files matching" should
-  "return a list with those files names" in {
-    val searchSubDirectories = true
-    val matcher = new Matcher("txt", new File(".\\testfiles\\").getCanonicalPath(), searchSubDirectories)
-    val results = matcher.execute()
-    assert(results == List("readme.txt", "notes.txt"))
+  describe("When matcher with sub folder checking matching a root location with two subtree files matching") {
+    it("Should return a list with those files names") {
+      val searchSubDirectories = true
+      val matcher = new Matcher("txt", new File(".\\testfiles\\").getCanonicalPath(), searchSubDirectories)
+      val results = matcher.execute()
+      assert(results == List("readme.txt", "notes.txt"))
+    }
   }
 }
